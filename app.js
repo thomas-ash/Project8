9a70ef3934d83b54415bee4e7a90cfd1bb983280
Thomas Ash <tommyash94@gmail.com>
2020-06-15 16:10:56 -0700

    fixed street name issue

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // GLOBAL -------------------------------
-const url = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`;
+const url = `https://randomuser.me/api/1.2/?results=12&nat=US`;
 let employees = [];
 const container = document.querySelector(".container");
 const overlay = document.querySelector(".overlay");
@@ -34,7 +33,7 @@ function displayEmployees(employeeData) {
             <div class="text-container">
                 <h2 class="name">${name.first} ${name.last}</h2>
                 <p class="email">${email}</p>
-                <p class="city">${city}</p>
+                <p class="address">${city}</p>
             </div>
         </div>
         `
@@ -44,20 +43,22 @@ function displayEmployees(employeeData) {
 }
 
 function displayModal(index) {
-   let { name, dob, phone, email, location: { city, street, state, postcode } , picture } = employees[index];
+    let { name, dob, phone, email, location: { street, city, state, postcode }, picture } = employees[index];
+    
     let date = new Date(dob.date);
 
     const modalHTML = `
-    <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
-        <h2 class="name">${name.first} ${name.last}</h2>
-        <p class="email">${email}</p>
-        <p class="city">${city}</p>
-        <hr />
-        <p>${phone}</p>
-        <p>${street}, ${state} ${postcode}</p>
-        <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-    </div> `;
+    <img class="avatar" src="${picture.large}"/>
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state} ${postcode}</p>
+            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div> `;
+
         overlay.classList.remove("hidden");
         modalContent.innerHTML = modalHTML;
 }
